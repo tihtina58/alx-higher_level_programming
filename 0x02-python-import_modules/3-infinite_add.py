c752bf27b9eaf674fb02e362dc82770185f1b232
tihtina58 <tihitnasete58@gmail.com>
2021-09-09 11:44:42 +0000

    import

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-  from sys import argv
-  sum = 0
-  length = len(argv)
-  if length == 1:
-     print("0")
-  else:
-     for i in argv[1:]:
-       sum = sum + int(i)
-     print("{}".format(sum))
+    from sys import argv
+    sum = 0
+    length = len(argv)
+    if length == 1:
+        print("0")
+    else:
+        for i in argv[1:]:
+            sum = sum + int(i)
+        print("{}".format(sum))
