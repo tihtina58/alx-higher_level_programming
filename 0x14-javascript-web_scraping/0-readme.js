b5b8766dd432042ea2076e1820bd5b362d3baa91
tihtina58 <tihitnasete58@gmail.com>
2022-02-02 20:25:18 +0000

    JS

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,12 +1,10 @@
 #!/usr/bin/node
-
-const file = process.argv[2];
 const fs = require('fs');
 
-fs.readFile(file, 'utf8', (err, data) => {
+fs.readFile(process.argv[2], 'utf-8', (err, data) => {
   if (err) {
-    console.error(err);
-    return;
+    console.log(err);
+  } else {
+    console.log(data.toString());
   }
-  console.log(data);
 });
